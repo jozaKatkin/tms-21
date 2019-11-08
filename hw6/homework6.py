2c6fb08b867618b49993850881afc1fab354bac5
jozaKatkin <mehojoey@gmail.com>
2019-11-08 19:06:29 +0300

    Find maximum element

--- hw6/homework6.py
+++ hw6/homework6.py
@@ -15,3 +15,11 @@ for i in range(m):
     my_matrix.append(line)
     print()
 # print(my_matrix)
+
+# Найти максимальный элемент матрицы
+max_num = my_matrix[0][0]
+for row in my_matrix:
+    for j in row:
+        if max_num < j:
+            max_num = j
+print(f"Maximum number: {max_num}")
