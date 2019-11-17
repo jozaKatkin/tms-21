b9803397d8a2c949d15d38b64a094db90c1c416a
jozaKatkin <mehojoey@gmail.com>
2019-11-17 21:17:37 +0300

    Classwork is done

---
+++ cw7/task_7_8.py
@@ -0,0 +1,25 @@
+def count_mean(*args, mean_type):
+    if mean_type == "arithmetical":
+        def arithmetical(some_args):
+            add = 0
+            for arg in some_args:
+                add += arg
+            arithm_mean = add / (len(some_args))
+            return arithm_mean
+
+        res = arithmetical(args)
+    elif mean_type == "geometrical":
+        def geometrical(some_args):
+            multi = 1
+            for arg in some_args:
+                multi *= arg
+            geom_mean = multi ** (1 / len(some_args))
+            return geom_mean
+
+        res = geometrical(args)
+
+    return res
+
+
+print(count_mean(1, 2, 3, 4, mean_type="arithmetical"))
+print(count_mean(1, 2, 3, 4, mean_type="geometrical"))
