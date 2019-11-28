83adb0b22be97b5747af85c4ef2c2315152b0179
jozaKatkin <mehojoey@gmail.com>
2019-11-28 19:58:16 +0300

    add methods to class

--- cw11/task_11.py
+++ cw11/task_11.py
@@ -1,11 +1,20 @@
 class Dog:
-    pass
+    def bark(self):
+        print("Woof-woof!")
+
+    def jump(self):
+        print("Jump!")
+
+    def run(self):
+        print("Run!")
+
 
 def main():
     dog1 = Dog()
     print(dog1)
     dog2 = Dog()
     print(dog2)
+    dog1.bark()
 
 
 if __name__ == "__main__":
