1b3d6520a52f05321d805e75c8cfd46deb2e2c03
jozaKatkin <mehojoey@gmail.com>
2019-11-28 21:02:11 +0300

    create set method to change private attribute

--- cw11/task_11.py
+++ cw11/task_11.py
@@ -1,13 +1,23 @@
 class Dog:
-    def __init__(self, name, age, height, weight, master):
+    def __init__(self, name, age, height, weight, master, address="Minsk"):
         self.name = name
         self.age = age
         self.height = height
         self.weight = weight
         self.__master = master
+        self.__address = address
 
     def get_master(self):
         return self.__master
+
+    def set_master(self, master):
+        self.__master = master
+
+    def get_address(self):
+        return self.__address
+
+    def set_address(self, address):
+        self.__address = address
 
     def change_name(self, name):
         self.name = name
@@ -28,6 +38,8 @@ class Dog:
 def main():
     dog1 = Dog("Bob", 3, 40, 3, "Joza")
     print(f"Master: {dog1.get_master()}")
+    dog1.set_master("Katkin")
+    print(f"New master: {dog1.get_master()}")
     dog1.bark()
     dog1.jump()
     dog1.run()
@@ -37,6 +49,9 @@ def main():
     dog1.change_name("Charlie")
     print(f"New height: {dog1.height}")
     print(f"New name: {dog1.name}")
+    print(f"Address: {dog1.get_address()}")
+    dog1.set_address("Kiev")
+    print(f"New address: {dog1.get_address()}")
 
 
 if __name__ == "__main__":
