4f54ffcd5684f2ec9aeb447fed21e7c01bd6d708
jozaKatkin <mehojoey@gmail.com>
2019-11-28 20:12:20 +0300

    create atributes in a class and call them

--- cw11/task_11.py
+++ cw11/task_11.py
@@ -1,4 +1,10 @@
 class Dog:
+    def __init__(self, name, age, height, weight, ):
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = weight
+
     def bark(self):
         print("Woof-woof!")
 
@@ -10,11 +16,12 @@ class Dog:
 
 
 def main():
-    dog1 = Dog()
-    print(dog1)
-    dog2 = Dog()
-    print(dog2)
+    dog1 = Dog("Bob", 3, 40, 3)
     dog1.bark()
+    dog1.jump()
+    dog1.run()
+    print(dog1.name)
+    print(dog1.age)
 
 
 if __name__ == "__main__":
