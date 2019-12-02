01d2cd9c7c52fad5abb6ac61bf2dad10312a458b
jozaKatkin <mehojoey@gmail.com>
2019-12-02 19:58:12 +0300

    change jump im children classes

---
+++ cw12/task_12_01.py
@@ -0,0 +1,82 @@
+class Pet:
+    def __init__(self, name, age, master, weight, height):
+        self.name = name
+        self.age = age
+        self.master = master
+        self.weight = weight
+        self.height = height
+
+    def change_weight(self, diff=0.2):
+        self.weight += diff
+
+    def change_height(self, height):
+        self.height = height
+        if height:
+            self.height = height
+        else:
+            self.height += 0.2
+
+    def run(self):
+        print("Run!")
+
+    def jump(self, height):
+        print(f"Jump\n{height}!")
+
+    def sleep(self):
+        print("Sleep...")
+
+    def birthday(self):
+        self.age += 1
+
+
+class Dog(Pet):
+    def bark(self):
+        print("Woof-woof")
+
+    def jump(self, height):
+        if height > 0.5:
+            print("Dogs cannot jump so high")
+        else:
+            super().jump(height)
+
+
+class Cat(Pet):
+    def meow(self):
+        print("Meow")
+
+    def jump(self, height):
+        if height > 0.5:
+            print("Cats cannot jump so high")
+        else:
+            super().jump(height)
+
+
+class Parrot(Pet):
+    def fly(self):
+        if self.weight > 0.1:
+            print("This parrot cannot fly")
+        else:
+            print("Flies away...")
+
+    def jump(self, height):
+        if height > 0.5:
+            print("Parrots cannot jump so high")
+        else:
+            super().jump(height)
+
+    def change_weight(self, diff=0.05):
+        self.weight += diff
+
+    def change_height(self, height=0.05):
+        self.height += height
+
+
+#
+parrot = Parrot("Yasha", 3, "Joza", 0.2, 10)
+print(parrot.weight)
+parrot.change_weight()
+print(parrot.weight)
+parrot.jump(0.6)
+#
+dog = Dog("Yasha", 3, "Joza", 0.2, 10)
+dog.jump(0.5)
