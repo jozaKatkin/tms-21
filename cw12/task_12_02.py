f21c75527ef937805067dd75e70273595d3a224b
jozaKatkin <mehojoey@gmail.com>
2019-12-06 22:36:50 +0300

    fix

--- cw12/task_12_02.py
+++ cw12/task_12_02.py
@@ -1,9 +1,7 @@
 class MyTime:
     def __init__(self, *args):
         if len(args) == 3:
-            self.hours = args[0]
-            self.minutes = args[1]
-            self.seconds = args[2]
+            self.hours, self.minutes, self.seconds = args
         elif len(args) == 1 and isinstance(args[0], str):
             self.hours, self.minutes, self.seconds = map(int, args[0].split("-"))
         elif len(args) == 1 and isinstance(args[0], MyTime):
@@ -11,67 +9,77 @@ class MyTime:
             self.hours, self.minutes, self.seconds = my_time.hours, my_time.minutes, my_time.seconds
         else:
             self.hours, self.minutes, self.seconds = 0, 0, 0
-
-        if self.seconds > 59:
-            self.seconds = self.seconds % 60
-            self.minutes = self.seconds // 60
-        if self.minutes > 59:
-            self.minutes = self.minutes % 60
-            self.hours = self.minutes // 60
+        self.hours, self.minutes, self.seconds = self.seconds_to_time(self.time_to_seconds())
 
     def __eq__(self, other):
-        return self.hours and self.minutes and self.seconds == other.hours and other.minutes and other.seconds
+        return all([
+            self.hours == other.hours,
+            self.minutes == other.minutes,
+            self.seconds == other.seconds
+        ])
+
+    def time_to_seconds(self):
+        return self.hours * 3600 + self.minutes * 60 + self.seconds
+
+    def seconds_to_time(self, seconds):
+        hours = seconds // 3600
+        minutes = seconds // 60 % 60
+        seconds = seconds % 60
+        return hours, minutes, seconds
 
     def __ne__(self, other):
-        return not self == other
+        return not (self == other)
 
     def __add__(self, other):
-        self.hours += other.hours
-        self.minutes += other.minutes
-        self.seconds += other.seconds
-        if self.seconds > 59:
-            self.seconds = self.seconds % 60
-            self.minutes = self.seconds // 60
-        if self.minutes > 59:
-            self.minutes = self.minutes % 60
-            self.hours = self.minutes // 60
-        return self.hours, self.minutes, self.seconds
+        total_seconds = self.time_to_seconds() + other.time_to_seconds()
+        hours, minutes, seconds = self.seconds_to_time(total_seconds)
+        return MyTime(hours, minutes, seconds)
 
     def __sub__(self, other):
-        return self.hours - other.hours, self.minutes - other.minutes, self.seconds - other.seconds
+        total_seconds = self.time_to_seconds() - other.time_to_seconds()
+        hours, minutes, seconds = self.seconds_to_time(total_seconds)
+        return MyTime(hours, minutes, seconds)
 
     def __str__(self):
         s = f'{self.hours}-{self.minutes}-{self.seconds}'
         return s
 
     def __lt__(self, other):
-        return self.hours and self.minutes and self.seconds < other.hours and other.minutes and other.seconds
+        return self.time_to_seconds() < other.time_to_seconds()
 
     def __gt__(self, other):
-        return self.hours and self.minutes and self.seconds > other.hours and other.minutes and other.seconds
+        return self.time_to_seconds() > other.time_to_seconds()
 
     def __le__(self, other):
-        return self.hours and self.minutes and self.seconds <= other.hours and other.minutes and other.seconds
+        return self.time_to_seconds() <= other.time_to_seconds()
 
     def __ge__(self, other):
-        return self.hours and self.minutes and self.seconds >= other.hours and other.minutes and other.seconds
+        return self.time_to_seconds() >= other.time_to_seconds()
 
     def __mul__(self, other):
-        self.hours *= other
-        self.minutes *= other
-        self.seconds *= other
-        if self.seconds > 59:
-            self.seconds = self.seconds % 60
-            self.minutes = self.seconds // 60
-        if self.minutes > 59:
-            self.minutes = self.minutes % 60
-            self.hours = self.minutes // 60
-        return self.hours, self.minutes, self.seconds
+        total_seconds = self.time_to_seconds() + other
+        hours, minutes, seconds = self.seconds_to_time(total_seconds)
+        return MyTime(hours, minutes, seconds)
 
 
 def main():
-    my_time1 = MyTime(1, 1, 1)
-    print(my_time1 * 70)
+    default_time = MyTime()
+    print(default_time)
+    int_time = MyTime(12, 55, 12)
+    print(int_time)
+    str_time = MyTime('2-66-55')
+    print(str_time)
+
+    print(int_time + str_time)
+    print(int_time - str_time)
+    print(str_time * 2)
+
+    print(str_time == int_time)
+    print(str_time != int_time)
+    print(str_time >= int_time)
+    print(str_time <= int_time)
+    print(str_time > int_time)
+    print(str_time < int_time)
 
 
 if __name__ == "__main__":
