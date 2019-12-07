d52b0c688d6cad616a2e6197dbeb7f77b9282152
jozaKatkin <mehojoey@gmail.com>
2019-12-07 23:36:10 +0300

    hw13_01 - calculator

---
+++ src/prog_utils/func.py
@@ -0,0 +1,15 @@
+def plus(num1, num2):
+    return num1 + num2
+
+
+def minus(num1, num2):
+    return num1 - num2
+
+
+def multi(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    return num1 / num2
+
---
+++ src/prog_utils/ui_func.py
@@ -0,0 +1,45 @@
+from func import (
+plus,
+minus,
+multi,
+divide
+)
+
+from exceptions import (
+BadCharError,
+Exit
+)
+
+
+while True:
+    try:
+        x = float(input('x: '))
+        y = float(input('y: '))
+    except ValueError:
+        print("Type in only numbers")
+        continue
+    sign = input('sign: ')
+    if sign == '0':
+        raise Exit
+    try:
+        if sign not in ['+', '-', '*', '/']:
+            raise BadCharError
+    except BadCharError:
+        print("Wrong input\n+, -, *, / signs expected")
+        continue
+    if sign == '/':
+        try:
+            if y == 0:
+                raise ZeroDivisionError
+        except ZeroDivisionError:
+            print("Cannot divide by zero")
+            continue
+        z = x / y
+    elif sign == '*':
+        z = x * y
+    elif sign == '+':
+        z = x + y
+    elif sign == '-':
+        z = x - y
+    print(f'{x} {sign} {y} = {z}')
+
